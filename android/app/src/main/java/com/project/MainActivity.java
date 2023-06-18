c4cd966eb30812053eb007433410463309780a4e
Jawad Raza <jawadraza8372@gmail.com>
2023-06-18 17:23:15 +0500

    changing package name in adroid

--- android/app/src/main/java/com/project/MainActivity.java
+++ android/app/src/main/java/com/project/MainActivity.java
@@ -1,4 +1,4 @@
-package com.project;
+package com.exclusiveappproject;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
--- android/app/src/main/java/com/project/MainApplication.java
+++ android/app/src/main/java/com/project/MainApplication.java
@@ -1,4 +1,4 @@
-package com.project;
+package com.exclusiveappproject;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
